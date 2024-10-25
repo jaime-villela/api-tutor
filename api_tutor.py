ed824dfe8d62c0fbc5c2c04ed1aa6f20c47a9e9e
Jaime Villela <jaime.villela@gmail.com>
2024-10-25 10:12:14 -0500

    Changed voice using voice ID

--- api_tutor.py
+++ api_tutor.py
@@ -10,7 +10,8 @@ client = ElevenLabs(api_key=api_key)
 
 audio = client.generate(
   text="Hello! 你好! Hola! नमस्ते! Bonjour! こんにちは! مرحبا! 안녕하세요! Ciao! Cześć! Привіт! வணக்கம்!",
-  voice="Brian",
+  # Changed voice to "Denzel - Casual Narration".  Typing the nane did not work, so I used the voice ID.
+  voice="nsQAxyXwUKBvqtEK9MfK",
   model="eleven_multilingual_v2"
 )
 play(audio)
