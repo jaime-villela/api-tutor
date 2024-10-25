edfabc1f33fe6f0fc3139ccf3144f6af83334907
Jaime Villela <45246367+jaime-villela@users.noreply.github.com>
2024-10-25 13:33:30 +0000

    First commit and functional state

---
+++ api_tutor.py
@@ -0,0 +1,17 @@
+import os
+from elevenlabs import play
+from elevenlabs.client import ElevenLabs
+
+api_key = os.getenv("ELEVEN_API_KEY")
+if not api_key:
+    raise ValueError("ELEVEN_API_KEY environment variable not set")
+
+print(api_key)
+client = ElevenLabs(api_key=api_key)
+
+audio = client.generate(
+  text="Hello! 你好! Hola! नमस्ते! Bonjour! こんにちは! مرحبا! 안녕하세요! Ciao! Cześć! Привіт! வணக்கம்!",
+  voice="Brian",
+  model="eleven_multilingual_v2"
+)
+play(audio)
